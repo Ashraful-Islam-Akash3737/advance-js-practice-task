c7f3210ab8b9ae72be725041970b89a542223b1a
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-02-29 20:12:28 +0600

    solve 2 problems

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('akash');
+// problem 1:
+
+// setTimeout(()=>{
+//     console.log('Akash');
+// }, 5000)
+
+
+// problem 2
+let time2 = 2000;
+function delayedGreeting (name, time){
+    console.log(`hello ${name}`);
+}
+const display = () => delayedGreeting("Alice!", 2000);
+setTimeout(display, time2)
