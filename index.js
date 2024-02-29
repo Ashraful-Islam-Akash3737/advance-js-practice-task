39a1322d2caac53eccfd6cd6b09640dec6f8447f
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-02-29 20:28:12 +0600

    solve  problems 4

--- index.js
+++ index.js
@@ -15,10 +15,27 @@
 
 
 // problem 3
-function tellJoke(){
-    console.log('joke');
+// function tellJoke(){
+//     console.log('joke');
+// }
+// const jokeInterval = setInterval(tellJoke, 2000);
+// setTimeout(()=>{
+//     clearInterval(jokeInterval);
+// }, 10000)
+
+
+// problem 4
+const loadData = async () => {
+    const res = await fetch('https://v2.jokeapi.dev/joke/Programming?type=single');
+    const data = await res.json();
+    console.log(data);
+    const funnyJoke = data.joke;
+    console.log(funnyJoke);
+    joke(funnyJoke);
 }
-const jokeInterval = setInterval(tellJoke, 2000);
-setTimeout(()=>{
-    clearInterval(jokeInterval);
-}, 10000)
+
+const joke = (funnyJoke) => {
+    const jokeContainer = document.getElementById('joke');
+    jokeContainer.innerText = funnyJoke;
+}
+loadData();
